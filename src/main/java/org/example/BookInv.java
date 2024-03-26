3d7621642ad1dc525aa99a6f49b8a83d7b00bd53
MennaHG <mennaahmed.ma54@gmail.com>
2024-03-26 15:36:03 +0200

    input validation

--- src/main/java/org/example/BookInv.java
+++ src/main/java/org/example/BookInv.java
@@ -46,18 +46,20 @@ public class BookInv {
 
 
 
-    public List<String> search(String res[]){
+    public List<String> search(String res[]) throws Exception{
+        if(res.length != 2)
+            throw new Exception("Invalid Input.Try again");
         String category=res[0]; String name=res[1];
-        // TODO change this to List<String>
         List<String> booksDoc = db.searchBooksBy(category,name);
         List<String> resultList = format(booksDoc);
         return resultList;
     }
 
      
-    // add title author desc , genres, price, quantity
     public void add(String username,String[] input) throws Exception{
-    
+        if(input.length != 6)
+            throw new Exception("Invalid Input.Try again");
+
         String title=input[0],author=input[1],  description=input[2];
         String genres[] = input[3].split(" ");
         float price=Float.parseFloat(input[4]);
@@ -68,8 +70,10 @@ public class BookInv {
             throw new Exception("Book already exists in user's library");
         }
 
-    public void remove(String username ,String booktitle) throws Exception{
-
+    public void remove(String username ,String res[]) throws Exception{
+        if(res.length != 1)
+            throw new Exception("Invalid Input.Try again");
+        String booktitle = res[0];
         if(!db.doesBookExist(username,booktitle))
             throw new Exception("Book does not exist");
         
